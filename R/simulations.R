a7b2c37db98a7b453b0c80ae3f421eabef3a97f5
Hector <hectorm94@gmail.com>
2026-06-01 12:47:04 +0200

    update sim_mvcomm

--- R/simulations.R
+++ R/simulations.R
@@ -26,95 +26,6 @@
 #' sim_mvcomm(n_sp = 15, years = 30)
 #' @export
 sim_mvcomm <- function(n_sp = 10,
-                       years = 25,
-                       tot_abu = 200 * n_sp,
-                       power = 1.8,
-                       corr = 0,
-                       even = 0.5,
-                       trend_mean = 0,
-                       trend_sd = 0.01){
-  
-  # Vector of mean abundances.
-  # check evenness values
-  if ( length(even) == 1 ) {
-    even <- ifelse(even == 0, even+0.01, even) # values == 0 give error
-    mean_abu <- sort(
-      tot_abu * geom_seq(max_rel_abu = even, n_sp = n_sp),
-      decreasing = TRUE
-    )
-  } else {
-    if ( length(even) != n_sp ) { stop("The length of the vector of relative abundances and the number of species differ.") }
-    if ( sum(even) != 1 ) { stop("The vector of relative abundances do not add up to 1.") }
-    mean_abu <- sort(
-      tot_abu * even,
-      decreasing = TRUE
-    )
-  }
-  
-  # Create a matrix of abundances
-  abu_matrix <- matrix(rep(mean_abu, times = years),
-                       nrow = years, ncol = n_sp, byrow = TRUE)
-  
-  # Simulate trends CHECK THIS OUT
-  trend <- seq(-1, 1, length.out = years)
-  # Check vector of trends
-  if( length(trend_mean) == 1 ){
-    trend_resp <- stats::rnorm(n = n_sp, mean = trend_mean, sd = trend_sd)
-  } else {
-    if ( length(trend_mean) != length(trend_sd) ){stop("Lengths of vectors of means and SD differ.")}
-    trend_resp <- sapply(seq_along(trend_mean), FUN = function(z){
-      stats::rnorm(n = 1, mean = trend_mean[z], sd = trend_sd[z])
-    }
-    )
-  }
-  
-  # Check that correlation is feasible
-  eta_min <- -1 / (n_sp - 1)
-  n_sp_max <- ceiling((-1 / corr) + 1)
-  if(corr < eta_min | corr > 1){
-    stop(paste0("correlation value must be between ", round(eta_min, 3), " and 1 or n_sp lower than ", n_sp_max))
-  }
-  
-  # Simulate random variation around mean abundance for each species
-  # drawn from multivariate normal so species correlate
-  simcom <- matrix(0, years, n_sp)
-  for (j in 1:years) {
-    mu <- abu_matrix[j,] *  ( 1 + trend[j] * trend_resp )
-    # Get SD of abundances from TPL
-    sd_abu <- sqrt(mu ** power)
-    abi <- unlist(faux::rnorm_multi(n = 1, 
-                                    mu = mu, 
-                                    sd = sd_abu,
-                                    r = corr))
-    # Force positive values
-    abi <- pmax(abi, 0)
-    simcom[j, ] <- abi
-  }
-  
-  # Add a small offset (1% of the mean abundance of each species) to avoid having 0s
-  off <- colMeans(simcom)*0.01 
-  # Add vector to matrix rowwise
-  simcom <- as.data.frame(sweep(x = simcom, MARGIN = 2, STATS = off, FUN = "+"))
-  
-  # Set species names
-  colnames(simcom) <- paste(sep = "_", "sp", seq_len(n_sp))
-  
-  # Results into list
-  res <- list(sim_data = simcom,
-              true_trend = colMeans(apply(log(simcom), 2, diff)),
-              params = c(n_sp = n_sp,
-                         years = years,
-                         tot_abu = tot_abu,
-                         power = power,
-                         corr = corr,
-                         even = even,
-                         trend_mean = unique(trend_mean),
-                         trend_sd = unique(trend_sd)))
-  
-  return(res)
-}
-
-sim_mvcomm2 <- function(n_sp = 10,
                         years = 25,
                         tot_abu = 200 * n_sp,
                         power = 1.8,
@@ -438,3 +349,92 @@ response <- function(state = TRUE,
   }
   return(resp)
 }
+
+# sim_mvcomm <- function(n_sp = 10,
+#                        years = 25,
+#                        tot_abu = 200 * n_sp,
+#                        power = 1.8,
+#                        corr = 0,
+#                        even = 0.5,
+#                        trend_mean = 0,
+#                        trend_sd = 0.01){
+#   
+#   # Vector of mean abundances.
+#   # check evenness values
+#   if ( length(even) == 1 ) {
+#     even <- ifelse(even == 0, even+0.01, even) # values == 0 give error
+#     mean_abu <- sort(
+#       tot_abu * geom_seq(max_rel_abu = even, n_sp = n_sp),
+#       decreasing = TRUE
+#     )
+#   } else {
+#     if ( length(even) != n_sp ) { stop("The length of the vector of relative abundances and the number of species differ.") }
+#     if ( sum(even) != 1 ) { stop("The vector of relative abundances do not add up to 1.") }
+#     mean_abu <- sort(
+#       tot_abu * even,
+#       decreasing = TRUE
+#     )
+#   }
+#   
+#   # Create a matrix of abundances
+#   abu_matrix <- matrix(rep(mean_abu, times = years),
+#                        nrow = years, ncol = n_sp, byrow = TRUE)
+#   
+#   # Simulate trends CHECK THIS OUT
+#   trend <- seq(-1, 1, length.out = years)
+#   # Check vector of trends
+#   if( length(trend_mean) == 1 ){
+#     trend_resp <- stats::rnorm(n = n_sp, mean = trend_mean, sd = trend_sd)
+#   } else {
+#     if ( length(trend_mean) != length(trend_sd) ){stop("Lengths of vectors of means and SD differ.")}
+#     trend_resp <- sapply(seq_along(trend_mean), FUN = function(z){
+#       stats::rnorm(n = 1, mean = trend_mean[z], sd = trend_sd[z])
+#     }
+#     )
+#   }
+#   
+#   # Check that correlation is feasible
+#   eta_min <- -1 / (n_sp - 1)
+#   n_sp_max <- ceiling((-1 / corr) + 1)
+#   if(corr < eta_min | corr > 1){
+#     stop(paste0("correlation value must be between ", round(eta_min, 3), " and 1 or n_sp lower than ", n_sp_max))
+#   }
+#   
+#   # Simulate random variation around mean abundance for each species
+#   # drawn from multivariate normal so species correlate
+#   simcom <- matrix(0, years, n_sp)
+#   for (j in 1:years) {
+#     mu <- abu_matrix[j,] *  ( 1 + trend[j] * trend_resp )
+#     # Get SD of abundances from TPL
+#     sd_abu <- sqrt(mu ** power)
+#     abi <- unlist(faux::rnorm_multi(n = 1, 
+#                                     mu = mu, 
+#                                     sd = sd_abu,
+#                                     r = corr))
+#     # Force positive values
+#     abi <- pmax(abi, 0)
+#     simcom[j, ] <- abi
+#   }
+#   
+#   # Add a small offset (1% of the mean abundance of each species) to avoid having 0s
+#   off <- colMeans(simcom)*0.01 
+#   # Add vector to matrix rowwise
+#   simcom <- as.data.frame(sweep(x = simcom, MARGIN = 2, STATS = off, FUN = "+"))
+#   
+#   # Set species names
+#   colnames(simcom) <- paste(sep = "_", "sp", seq_len(n_sp))
+#   
+#   # Results into list
+#   res <- list(sim_data = simcom,
+#               true_trend = colMeans(apply(log(simcom), 2, diff)),
+#               params = c(n_sp = n_sp,
+#                          years = years,
+#                          tot_abu = tot_abu,
+#                          power = power,
+#                          corr = corr,
+#                          even = even,
+#                          trend_mean = unique(trend_mean),
+#                          trend_sd = unique(trend_sd)))
+#   
+#   return(res)
+# }
